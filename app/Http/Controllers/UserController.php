d21f35e39a9d95486b8f9b53f6721bcfe1ec3458
papa28x4 <swissfair@yahoo.co.uk>
2023-07-02 15:43:58 +0100

    add dashboard gate

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,9 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Admin;
 use App\Models\User;
+use Illuminate\Contracts\Auth\Access\Gate;
 
 class UserController extends Controller
 {
+
+    public function __construct(Gate $gate)
+    {
+        //We can define it here since we are just using it in this controller alone
+        $gate->define('see-dashboard', fn ($user) => $user->hasRole('Admin'));
+        
+        // Now, we can just simply call the authorization middleware like normal.
+        $this->middleware('can:see-dashboard');
+    }
+    
     public function dashboard()
     {
         $users =  User::whereHas('roles', function($q){
